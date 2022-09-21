b8c3e353d1258dcab37262cf67a21690c9e254a4
uihoh0 <haofeng.zhao@leyantech.com>
2022-09-21 15:52:31 +0800

    feat:  pass through all the config when needed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,10 @@
+## `@1.1.1`
+  
+  -  pass through all the config when needed
+
 ## `@1.1.0`
   
   - support the change of `eletron-builder@21`
-
-
 ## `@1.0.2`
 -------------
 Fix a bug which cause by Using a wrong value of `platform` and `arch`(mostly wrong in windows).
--- src/index.ts
+++ src/index.ts
@@ -61,6 +61,8 @@ export default class AliOssPublisher extends HttpPublisher {
       "ali-oss-publisher:initial"
     );
     this.client = new OSS({
+      // pass through all the config when needed
+      ...config,
       region: config.region,
       //云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，部署在服务端使用RAM子账号或STS，部署在客户端使用STS。
       accessKeyId: config.accessKeyId,
